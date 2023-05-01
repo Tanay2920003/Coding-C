78412dbc039710d9dcf5a5d4e825b0a1b038ed62
Tanay <127974995+Tanay2920003@users.noreply.github.com>
2023-05-01 14:57:47 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{   /*
+
+
+multiline comment */
+int a=10; // dec+initiallization
+int b; //declaration
+b=100;//initialization
+int age=19;
+float work=99.9;
+double k=3.999999999999;
+char grade='A';
+char name[]="Bro,Tanay";
+bool w=true;
+char f=100;
+    printf("Hello %s \n",name);//print
+    printf("You are %d years old\n",age);//print
+     printf("You are %d years old\n",age);//print
+      printf("Your average grade is %c  \n",grade);//print
+       printf("Your work is %f\n",work);//print
+       printf("Your team is %lf\n",k);//print
+       printf("Answer-%d\n",w);
+       printf("char 100-%c\n",f);
+    return 0;  
+
+}
